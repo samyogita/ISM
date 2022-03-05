a5dc04bb0f7b1b7dcb97160cb437a4a7984f74db
samyogita <bhandarisamyogita@gmail.com>
2022-03-05 16:44:47 +0545

    changed method to $_POST

--- ISM_Sqlmap/login.php
+++ ISM_Sqlmap/login.php
@@ -1,8 +1,8 @@
 <?php
 include('dbconn.php');
-$username = $_GET["username"];
-$password = $_GET["password"];
-$purchase = $_GET["purchase"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$purchase = $_POST["purchase"];
 $sql = "SELECT username FROM users WHERE username = '" + username + "'and password = '" + password + "'";
 $conn->query($sql);
 echo "Hello user";

73670355dbb53384142c3d010a4bab5eb0fb3189
samyogita <bhandarisamyogita@gmail.com>
2022-03-05 18:07:15 +0545

    can login only with correct credentials

--- ISM_Sqlmap/login.php
+++ ISM_Sqlmap/login.php
@@ -1,9 +1,25 @@
 <?php
-include('dbconn.php');
+$jj='';
+$pss='';
 $username = $_POST["username"];
 $password = $_POST["password"];
 $purchase = $_POST["purchase"];
-$sql = "SELECT username FROM users WHERE username = '" + username + "'and password = '" + password + "'";
-$conn->query($sql);
-echo "Hello user";
+$conn = mysqli_connect("localhost", "root", "", "ism");
+$sql = "SELECT * FROM users WHERE username = '$username' and password = '$password' and purchase = '$purchase'";
+$result=mysqli_query($conn, $sql);
+$num= mysqli_num_rows($result);
+if($num>0)
+{
+while( $row=mysqli_fetch_assoc($result))
+{
+if($username==$row['username'] and $password==$row['password'] and $purchase==$row['purchase'])
+echo "Hello", " ", $username,".";
+echo "Your purchase is", " ", $purchase;
+}
+}
+else{
+    echo "Incorrect username or password";
+    echo '<script>alert("Incorrect username or password")</script>';
+} 
+
 ?>
